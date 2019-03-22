02268d7fa102e03562609c9386104d4f1ba2379a
Jan Šmolík <loglim@loglim.cz>
2019-03-23 00:52:58 +0100

    Migrated project from Maven to Gradle, modified App class

--- src/main/java/cz/loglim/jrest/App.java
+++ src/main/java/cz/loglim/jrest/App.java
@@ -1,11 +1,11 @@
 package cz.loglim.jrest;
 
-import cz.loglim.jrest.dao.ItemDao;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.glassfish.jersey.servlet.ServletContainer;
 
+import java.net.InetSocketAddress;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -13,26 +13,25 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Server starting...");
-        Server server = new Server(8080);
-        ServletContextHandler ctx = new ServletContextHandler(ServletContextHandler.NO_SESSIONS);
-        ctx.setContextPath("/");
-        server.setHandler(ctx);
-
-        ServletHolder sh = ctx.addServlet(ServletContainer.class, "/api/*");
+        Server server = new Server(new InetSocketAddress("127.0.0.1", 8080));
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.NO_SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+        ServletHolder sh = context.addServlet(ServletContainer.class, "/api/*");
         sh.setInitOrder(1);
+        sh.setInitParameter("jersey.config.server.provider.classnames", "org.glassfish.jersey.jackson.JacksonFeature");
         sh.setInitParameter("jersey.config.server.provider.packages", "cz.loglim.jrest.controller");
 
         try {
             server.start();
             server.join();
         } catch (Exception ex) {
-            Logger.getLogger(App.class.getName()).log(Level.ALL, null, ex);
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
         } finally {
             server.destroy();
         }
 
-        ItemDao.finish();
-        System.out.println("Server finished!");
+        System.out.println("Server thread finsihed!");
     }
 
 }
